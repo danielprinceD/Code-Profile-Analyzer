b1ccabe992579b638bd9e814d62480c2a7519c0a
daniel <d.danielprince2003@gmail.com>
2024-03-06 20:32:39 +0530

    feat:Vercel for Backend

--- Backend/settings.py
+++ Backend/settings.py
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
